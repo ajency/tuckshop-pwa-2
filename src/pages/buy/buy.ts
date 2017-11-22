a24247309f38677d90f0b8045d1fc084793ae65b
sujitgawas19 <sujit@ajency.in>
2017-11-22 15:26:47 +0530

    New client id

--- src/pages/buy/buy.ts
+++ src/pages/buy/buy.ts
@@ -41,11 +41,14 @@ handleClientLoad() {
       // let that = this;
         gapi.load('client:auth2', function () {
         gapi.client.init({
-           client_id: '676621258132-6q9s2j1hc8343jj3nn75k0is4s1nb893.apps.googleusercontent.com',
+           client_id: '676621258132-v9fqtd0tl50in48cmb92jht0k3be2024.apps.googleusercontent.com',
+          // client_id: '676621258132-6q9s2j1hc8343jj3nn75k0is4s1nb893.apps.googleusercontent.com',
            // client_id: '164623832984-ivug8glc6tgtu0sgjbm51oigp27u0033.apps.googleusercontent.com',
 
         cookiepolicy: 'single_host_origin',
         scope: 'https://www.googleapis.com/auth/spreadsheets'
+        // scope: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+        
         }).then(function () {
 
           // Listen for sign-in state changes.
