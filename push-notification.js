0c746f70e7abfffb70c9f800a937ab4eb68d2804
sujitgawas19 <sujit@ajency.in>
2018-07-02 14:52:40 +0530

    notification on click event changed

--- push-notification.js
+++ push-notification.js
@@ -21,24 +21,25 @@ messaging.setBackgroundMessageHandler(function(payload) {
 	  console.log('On notification click: ', event.notification.tag);  
 	  // Android doesn't close the notification when you click on it  
 	  // See: http://crbug.com/463146  
+	  clients.openWindow('https://tuckshop.ajency.in');
 	  event.notification.close();
 
 	  // This looks to see if the current is already open and  
 	  // focuses if it is  
-	  event.waitUntil(
-	    clients.matchAll({  
-	      type: "window"  
-	    })
-	    .then(function(clientList) {  
-	      for (var i = 0; i < clientList.length; i++) {  
-	        var client = clientList[i];  
-	        if (client.url == '/' && 'focus' in client)  
-	          return client.focus();  
-	      }  
-	      if (clients.openWindow) {
-	        return clients.openWindow('https://tuckshop.ajency.in');  
-	      }
-	    })
-	  );
+	  // event.waitUntil(
+	  //   clients.matchAll({  
+	  //     type: "window"  
+	  //   })
+	  //   .then(function(clientList) {  
+	  //     for (var i = 0; i < clientList.length; i++) {  
+	  //       var client = clientList[i];  
+	  //       if (client.url == '/' && 'focus' in client)  
+	  //         return client.focus();  
+	  //     }  
+	  //     if (clients.openWindow) {
+	  //       return clients.openWindow('https://tuckshop.ajency.in');  
+	  //     }
+	  //   })
+	  // );
 	});
 });
