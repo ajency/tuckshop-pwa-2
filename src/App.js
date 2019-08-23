7b28495af3620f4899134586df3675b78c4e5f3d
AJ-Sujit <sujit@ajency.in>
2019-08-23 16:02:21 +0530

    tuckshop react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './assets/img/tuckshop.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Tuckshop React App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
