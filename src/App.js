042c8d6b92315ee36e651a05dd318915130303b9
AJ-Sujit <sujit@ajency.in>
2019-08-30 12:48:43 +0530

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import Home from './components/home/home.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
-}
+import Home from './components/home/home.js';
+import List from './components/list-page/list-page.js';
+
+const App = () => (
+	<Router>
+		<div>
+			<Route exact path="/" component={Home} />
+			<Route path="/search" component={List} />
+		</div>
+	</Router>
+)
 
 export default App;
