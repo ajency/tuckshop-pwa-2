6cc194e3ceaa88d5e22405edc98b825704b339e7
AJ-Sujit <sujit@ajency.in>
2019-09-09 19:19:26 +0530

    production build

---
+++ build/precache-manifest.ef71a365801dbc7bb9cd7d256defee29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff5cd89d961c8ff41f5166638aa1bf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ddf0a988cd218ace2ef",
+    "url": "/static/css/2.c4d8cd00.chunk.css"
+  },
+  {
+    "revision": "ce13b18b1fa27c7dff5b",
+    "url": "/static/css/main.a8914cf2.chunk.css"
+  },
+  {
+    "revision": "6ddf0a988cd218ace2ef",
+    "url": "/static/js/2.224fc802.chunk.js"
+  },
+  {
+    "revision": "ce13b18b1fa27c7dff5b",
+    "url": "/static/js/main.3195ef90.chunk.js"
+  },
+  {
+    "revision": "e02f93ffda92062aa6b6",
+    "url": "/static/js/runtime~main.68cdd344.js"
+  },
+  {
+    "revision": "82cf9be4663c591a3091c3d912230e27",
+    "url": "/static/media/filter-sprite.82cf9be4.png"
+  },
+  {
+    "revision": "a4db8ae6b58172ac879959a26d0231d1",
+    "url": "/static/media/tuckshop.a4db8ae6.jpg"
+  }
+]);
