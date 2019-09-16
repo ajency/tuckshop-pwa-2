0ea1f5e0a3ceea7a0eb0dc7dbe05eb587a1bcd26
AJ-Sujit <sujit@ajency.in>
2019-09-16 12:23:31 +0530

    specials page complete

--- src/components/specials/specials.js
+++ src/components/specials/specials.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import firebaseApp from '../firebase/firebase.js';
 import Header from '../header/header.js';
+import './specials.scss'
 
 class Specials extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			db : firebaseApp.firestore(),
-			orders : []
+			specialsOrders : []
 		}
 	}
 
@@ -15,11 +16,29 @@ class Specials extends Component {
 		this.fetchOrders();
 	}
 
+	getUserImage(special){
+		return special.map((order) =>
+    			<img key={order.user_email + order.created} src={order.photoURL} width="70" className="avatar-img m-2 rounded-circle"/>
+			 )
+	}
+
 	render() {
 		let orderContainer;
-		if(this.state.orders.length)
-			orderContainer = this.state.orders.map((order) =>
-		    	<img key={order.user_email} src={order.photoURL} width="70" className="avatar-img m-2 rounded-circle"/>
+		if(this.state.specialsOrders.length)
+			orderContainer = this.state.specialsOrders.map((special) =>
+				<div className="special-wrapper mb-2" key={special[0].item_code}>
+					<div className="top-bar d-flex font-weight-bolder justify-content-between mb-2">
+						<div>
+							{special[0].item_name}
+						</div>
+						<div>
+							Order Count :{special.length}
+						</div>
+					</div>
+					<div>
+						{this.getUserImage(special)}
+			    	</div>
+			    </div>
 			);
 		else
 			orderContainer = <div className="text-center mt-5"> <h4> No Results </h4>  </div>
@@ -37,13 +56,28 @@ class Specials extends Component {
 	fetchOrders() {
 		this.state.db.collection("orders").onSnapshot(querySnapshot => {
 			  		let orders = querySnapshot.docs.map(doc => doc.data());
-			  		console.log("orders ==>", orders)
-			  		orders = orders.filter((order) => order.item_code === "400103")
+			  		console.log("orders ==>", orders, (orders[0].created.toDate()).toLocaleDateString(), new Date().toLocaleDateString() );
+			  		orders = orders.filter((order) => order.type === "Special" && (order.created.toDate()).toLocaleDateString() == new Date().toLocaleDateString())
 			  		orders.sort((a,b)=>{
 			  			return a.created.seconds - b.created.seconds;
 			  		})
-			  		this.setState({orders : orders});
+			  		this.groupByItem(orders, 'item_code');			  		
 	    });
+	}
+
+	groupByItem(array, key){
+		let grouped_obj = 
+			array.reduce((objectsByKeyValue, obj) => {
+				const value = obj[key];
+				objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+				return objectsByKeyValue;
+			}, {});
+		let grouped_array = []
+		for (var key in grouped_obj) {
+			grouped_array.push(grouped_obj[key]);
+		}
+		console.log("group by ==>", grouped_array);
+		this.setState({specialsOrders : grouped_array});
 	}
 }
 
